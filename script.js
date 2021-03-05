6107702ca6c3548a111f79b8ae177f144054987a
UpperSideBand <63129705+UpperSideBand@users.noreply.github.com>
2021-03-05 14:05:58 +0000

    High score implemented

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function checkGuess() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
 
-    if (1 < score) {
+    if (1 < score && !finished) {
         if (!guess) {
             document.querySelector('.message').textContent =
                 '⛔ No number entered...';
@@ -25,6 +25,11 @@ function checkGuess() {
             document.querySelector('body').style.backgroundColor =
                 'DarkOliveGreen';
             document.querySelector('.number').style.width = '30rem';
+            finished = true;
+            if (highScore < score) {
+                highScore = score;
+                document.querySelector('.highscore').textContent = highScore;
+            }
         } else if (0 < guess && guess < secretNumber) {
             document.querySelector('.message').textContent =
                 '📉 ' + guess + ' is too low...';
@@ -36,6 +41,7 @@ function checkGuess() {
         } else
             document.querySelector('.message').textContent =
                 '⛔ Out of range...';
+    } else if (finished) {
     } else {
         document.querySelector('.score').textContent = 0;
         document.querySelector('.message').textContent = '😭 Game over';
@@ -47,7 +53,7 @@ function checkGuess() {
 
 function reset() {
     secretNumber = Math.trunc(Math.random() * 20) + 1;
-    console.log(`SecretNumber ${secretNumber}`);
+    console.log(`Secret Number: ${secretNumber}`);
     score = 20;
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.guess').value = '';
@@ -55,10 +61,13 @@ function reset() {
     document.querySelector('.number').textContent = '?';
     document.querySelector('.number').style.width = '15rem';
     document.querySelector('.score').textContent = score;
+    finished = false;
 }
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
+let finished = false;
 console.log(`Secret Number: ${secretNumber}`);
 
 /* document.querySelector('.number').textContent = secretNumber;*/
